23abdaac787bcc0d21fcc8984254690fc577c7f5
Kamil Kalandarov <Unknownman131313@yandex.ru>
2022-10-14 23:14:25 +0500

    setion video maded without adoptadion

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const twoVideo = new Swiper('.video__slider', {
+  slideClass: 'video__slider-slide',
+  wrapperClass: 'video__slider-wrapper',
+  setWrapperSize: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 59,
+  allowTouchMove: true,
+  centeredSlide: true,
+});
+
+const threeVideo = new Swiper('.video__slider-wrapper_type_three', {
+  slideClass: 'video__slider-slide',
+  wrapperClass: 'video__slider-wrapper_type_three',
+  setWrapperSize: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 59,
+  allowTouchMove: true,
+  centeredSlide: true,
+});
+
+
